1a9c4e9cafc66b291cbb4e9179dd70e779e25330
adityaramteke <adityaramteke05icr@gmail.com>
2021-03-22 14:06:46 +0530

    [Test] Add TC to validate LVM inside container does not use udev
    This TC is to validate the service dmevent is not present in glusterfs
    container and LVM is not trying to use it.
    
    Signed-off-by: adityaramteke aramteke@redhat.com

--- tests/functional/heketi/test_heketi_lvm_wrapper.py
+++ tests/functional/heketi/test_heketi_lvm_wrapper.py
@@ -1,12 +1,16 @@
 import re
 
 import ddt
+from glusto.core import Glusto as g
 import pytest
 
-from openshiftstoragelibs import command
+
 from openshiftstoragelibs import baseclass
+from openshiftstoragelibs import command
+from openshiftstoragelibs import exceptions
 from openshiftstoragelibs import heketi_ops
 from openshiftstoragelibs import heketi_version
+from openshiftstoragelibs import node_ops
 from openshiftstoragelibs import openshift_ops
 from openshiftstoragelibs import openshift_version
 from openshiftstoragelibs import waiter
@@ -275,3 +279,126 @@ class TestHeketiLvmWrapper(baseclass.BaseClass):
 
         # Create PVC after docker restart
         self.create_and_wait_for_pvcs()
+
+    def _check_heketi_and_gluster_pod_after_node_reboot(self, node_name):
+        # Wait for heketi pod to become ready and running
+        heketi_pod = openshift_ops.get_pod_names_from_dc(
+            self.oc_node, self.heketi_dc_name)[0]
+        openshift_ops.wait_for_pod_be_ready(self.oc_node, heketi_pod)
+        heketi_ops.hello_heketi(self.oc_node, self.heketi_server_url)
+
+        # Wait for glusterfs pods to become ready if hosted on same node
+        heketi_node_ip = openshift_ops.oc_get_custom_resource(
+            self.oc_node, 'pod', '.:status.hostIP', heketi_pod)[0]
+        if heketi_node_ip in self.gluster_servers:
+            gluster_pod = openshift_ops.get_gluster_pod_name_for_specific_node(
+                self.oc_node, node_name)
+
+            # Wait for glusterfs pod to become ready
+            openshift_ops.wait_for_pod_be_ready(self.oc_node, gluster_pod)
+            services = (
+                ("glusterd", "running"), ("gluster-blockd", "running"),
+                ("tcmu-runner", "running"), ("gluster-block-target", "exited"))
+            for service, state in services:
+                openshift_ops.check_service_status_on_pod(
+                    self.oc_node, gluster_pod, service, "active", state)
+
+    @pytest.mark.tier2
+    def test_udev_usage_in_container(self):
+        """Validate LVM inside container does not use udev"""
+
+        # Skip the TC if independent mode deployment
+        if not self.is_containerized_gluster():
+            self.skipTest(
+                "Skipping this test case as it needs to run on "
+                "converged mode deployment")
+
+        h_client, h_url = self.heketi_client_node, self.heketi_server_url
+        server_info = list(g.config.get('gluster_servers').values())[0]
+        server_node = server_info.get('manage')
+        additional_device = server_info.get('additional_devices')[0]
+
+        # Get pod name from on host
+        for pod_info in self.pod_name:
+            if pod_info.get('pod_hostname') == server_node:
+                pod_name = pod_info.get('pod_name')
+                break
+
+        # Create file volume
+        vol_info = heketi_ops.heketi_volume_create(
+            h_client, h_url, self.volume_size, json=True)
+        self.addCleanup(
+            heketi_ops.heketi_volume_delete, h_client, h_url,
+            vol_info.get("id"))
+
+        # Create block volume
+        block_vol_info = heketi_ops.heketi_blockvolume_create(
+            h_client, h_url, self.volume_size, json=True)
+        self.addCleanup(
+            heketi_ops.heketi_blockvolume_delete, h_client, h_url,
+            block_vol_info.get("id"))
+
+        # Check dmeventd service in container
+        err_msg = "dmeventd.service is running on setup"
+        with self.assertRaises(AssertionError, msg=err_msg):
+            openshift_ops.oc_rsh(
+                self.oc_node, pod_name, "systemctl is-active dmeventd.service")
+
+        # Service dmeventd should not be running in background
+        with self.assertRaises(AssertionError, msg=err_msg):
+            openshift_ops.oc_rsh(
+                self.oc_node, pod_name, "ps aux | grep dmeventd.service")
+
+        # Perform a pvscan in contaier
+        openshift_ops.oc_rsh(
+            self.oc_node, pod_name, "pvscan")
+
+        # Get heketi node to add new device
+        heketi_node_list = heketi_ops.heketi_node_list(h_client, h_url)
+        for h_node_id in heketi_node_list:
+            h_node_info = heketi_ops.heketi_node_info(
+                h_client, h_url, h_node_id, json=True)
+            h_node_host = h_node_info.get('hostnames', {}).get('manage')[0]
+            if h_node_host == server_node:
+                break
+
+        # Add new device to the node
+        heketi_ops.heketi_device_add(
+            h_client, h_url, additional_device, h_node_id)
+        h_node_info = heketi_ops.heketi_node_info(
+            h_client, h_url, h_node_id, json=True)
+        h_device_id = [
+            device.get('id')
+            for device in h_node_info.get('devices')
+            if device.get('name') == additional_device]
+        self.addCleanup(
+            heketi_ops.heketi_device_delete, h_client, h_url, h_device_id[0])
+        self.addCleanup(
+            heketi_ops.heketi_device_remove, h_client, h_url, h_device_id[0])
+        self.addCleanup(
+            heketi_ops.heketi_device_disable, h_client, h_url, h_device_id[0])
+
+        # Reboot the node on which device is added
+        self.addCleanup(
+            self._check_heketi_and_gluster_pod_after_node_reboot, server_node)
+        node_ops.node_reboot_by_command(server_node)
+
+        # Wait node to become NotReady
+        custom = r'":.status.conditions[?(@.type==\"Ready\")]".status'
+        for w in waiter.Waiter(300, 10):
+            status = openshift_ops.oc_get_custom_resource(
+                self.oc_node, 'node', custom, server_node)
+            if status[0] == 'False':
+                break
+        if w.expired:
+            raise exceptions.ExecutionError(
+                "Failed to bring node down {}".format(server_node))
+
+        # Wait for node to become ready
+        openshift_ops.wait_for_ocp_node_be_ready(self.oc_node, server_node)
+
+        # Wait for heketi and glusterfs pod to become ready
+        self._check_heketi_and_gluster_pod_after_node_reboot(server_node)
+
+        # Perform a pvscan in contaier
+        openshift_ops.oc_rsh(self.oc_node, pod_name, "pvscan")
